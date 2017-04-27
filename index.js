36da0bdfca2bb4e19c96a3dd61bacd48b54c160f
John A Maloney <johnamaloney@Johns-MacBook-Pro-2.local>
2017-04-27 13:53:43 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 'use strict';
 
 var express = require('express')
-
+var bodyParser = require('body-parser')
 const app = express()
 
 const port = process.env.PORT || 3000;
 
+//support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'www')));
+
 app.route('/')
 	.get((req, res) => res.json())
-	.post((req, res) => res.body)
 
+
+app.post('/approve', (req, res) => console.log(req.body))
 
 app.listen(port, function() {
     console.log('App listening on port ' + port);
