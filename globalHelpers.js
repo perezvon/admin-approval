8b0c2f66e99c0f2e17597ffa65f728e01cbc167f
John A Maloney <johnamaloney@Johns-MacBook-Pro-2.local>
2017-04-27 14:54:31 -0500

    testing post status

--- globalHelpers.js
+++ globalHelpers.js
@@ -1,27 +1,35 @@
 function checkForSupervisor(customerID) {
-  const url = 'https://apirest.3dcart.com/3dCartWebAPI/v1/Customers/' + customerID;
-  const accessToken = process.env.TOKEN ? process.env.TOKEN : '87dcb88f1619747fd8398aa6731cac15';
-  const privateKey = process.env.KEY ? process.env.KEY : 'be6a6060c5b8d34baff6fef2d5902529';
-  let myInit = {
-    method: 'GET',
-    credentials: 'include',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'SecureUrl': 'https://717418968211.3dcart.net',
-      'PrivateKey': privateKey,
-      'Token': accessToken,
-      'cache-control': 'no-cache'
-    }
-  };
-  fetch(url, myInit)
-    .then(res => {
-      if (res.ok) return res.json();
-    })
-    .then(json => {
-      console.log(json)
-      return json;
-    })
+  var http = require("https");
+
+var options = {
+  "method": "GET",
+  "hostname": "apirest.3dcart.com",
+  "port": null,
+  "path": "/3dCartWebAPI/v1/Customers/2",
+  "headers": {
+    "accept": "application/json",
+    "content-type": "application/json;charset=UTF-8",
+    "secureurl": "https://717418968211.3dcart.net",
+    "token": "87dcb88f1619747fd8398aa6731cac15",
+    "privatekey": "be6a6060c5b8d34baff6fef2d5902529",
+    "cache-control": "no-cache"
+  }
+};
+
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.end();
 	//if (customer.adminEmailField) return true;
   return false;
 }
