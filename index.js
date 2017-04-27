1f1a5c8e996e0b5c4ca374be06b11ce4c2ebd57f
John A Maloney <johnamaloney@Johns-MacBook-Pro-2.local>
2017-04-27 13:40:23 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+app.route('/')
+	.get((req, res) => res.body())
+
+
+app.listen(port, function() {
+    console.log('App listening on port ' + port);
+});
