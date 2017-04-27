411674e127de26f3340851ca7e1ac9f28a9396c1
John A Maloney <johnamaloney@Johns-MacBook-Pro-2.local>
2017-04-27 15:53:23 -0500

    fixing async callbacks

--- globalHelpers.js
+++ globalHelpers.js
@@ -33,7 +33,35 @@ req.end();
 }
 
 function sendEmail(address) {
-	if (address) console.log('address: ' + address)
+	if (address) {
+    const nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+      user: 'john.a.maloney@gmail.com',
+      pass: process.env.EMAILPASS
+  }
+});
+
+// setup email data with unicode symbols
+let mailOptions = {
+  from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
+  to: address, // list of receivers
+  subject: 'Hello ✔', // Subject line
+  text: 'Hello world ?', // plain text body
+  html: '<b>Hello world ?</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+      return console.log(error);
+  }
+  console.log('Message %s sent: %s', info.messageId, info.response);
+});
+  }
   else return false;
 }
 
